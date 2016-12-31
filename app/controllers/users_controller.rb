cec5ee24f609830c7506fc5f5871adf27d25c23c
Min Khati <meetup.min@gmail.com>
2016-12-31 18:56:28 +0000

    Add users index view, action and other details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   
+  # GET /users
+  def index
+    @users = User.includes(:profile)
+  end
+  
   # GET /users/:id
   def show
     @user = User.find(params[:id])
