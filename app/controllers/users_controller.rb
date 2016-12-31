3872e4a0db1ae7cda492db515f8e7fad210bfee8
Min Khati <meetup.min@gmail.com>
2016-12-31 18:23:36 +0000

    Add before_action filter for correct user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,9 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  
   # GET /users/:id
   def show
     @user = User.find(params[:id])
-  end
-  
-  def create
-     
   end
    
 end
